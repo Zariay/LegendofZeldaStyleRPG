5de8891a5e2ea8e366d59285f51a523867aca300
Zariay <alexswfan@gmail.com>
2018-12-03 15:09:56 -0800

    AnimationStartPlayer

---
+++ Legend of Zelda Style RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Vector2 direction;
+
+    public Animator animator;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        direction = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+        if(Input.GetKey(KeyCode.A))
+        {
+            
+            direction += Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            
+            direction += Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("WKey", true);
+            direction += Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("SKey", true);
+            direction += Vector2.down;
+        }
+        else
+        {
+            animator.SetBool("WKey", false);
+            animator.SetBool("SKey", false);
+        }
+        
+    }
+}
