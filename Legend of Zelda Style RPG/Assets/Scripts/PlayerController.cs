c41395f0b841e9393bf4f829d029a7f4e651efb5
Zariay <alexswfan@gmail.com>
2018-12-03 16:01:09 -0800

    Need to work on Animation smoothing/transitioning

--- Legend of Zelda Style RPG/Assets/Scripts/PlayerController.cs
+++ Legend of Zelda Style RPG/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,16 @@ public class PlayerController : MonoBehaviour
     private Vector2 direction;
 
     public Animator animator;
-    
+
+    SpriteRenderer spriteRenderer;
+    [SerializeField]
+    Sprite leftSprite, rightSprite, downSprite, upSprite;
 
     // Use this for initialization
     void Start()
     {
         direction = Vector2.zero;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -34,26 +38,32 @@ public class PlayerController : MonoBehaviour
         direction = Vector2.zero;
         if(Input.GetKey(KeyCode.A))
         {
-            
+            animator.SetBool("AKey", true);
             direction += Vector2.left;
+            spriteRenderer.sprite = leftSprite;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            
+            animator.SetBool("DKey", true);
             direction += Vector2.right;
+            spriteRenderer.sprite = rightSprite;
         }
         else if (Input.GetKey(KeyCode.W))
         {
             animator.SetBool("WKey", true);
             direction += Vector2.up;
+            spriteRenderer.sprite = upSprite;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             animator.SetBool("SKey", true);
             direction += Vector2.down;
+            spriteRenderer.sprite = downSprite;
         }
         else
         {
+            animator.SetBool("AKey", false);
+            animator.SetBool("DKey", false);
             animator.SetBool("WKey", false);
             animator.SetBool("SKey", false);
         }
